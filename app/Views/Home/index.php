166eb3e739bba23f0b7e2552010dae28bc10a4e2
AsukaGames <asukagamedev@gmail.com>
2023-09-05 21:59:38 +0200

    view layout default
    Création d'un view layout (template de page réutilsable) avec le header, footer du site, etc.

---
+++ app/Views/Home/index.php
@@ -0,0 +1,7 @@
+<?= $this->extend("layouts/default") ?>
+
+<?= $this->section("titre") ?>Page de connexion - GSB<?= $this->endSection() ?>
+
+<?= $this->section("contenu") ?>
+	<p>Bonjour ! Le site est en construction.</p>
+<?= $this->endSection() ?>
